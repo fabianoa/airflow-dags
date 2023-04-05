5afa89f6b357f73af759087553ee0b2b7e319591
Fabiano Alencar <fabiano.alencar@gmail.com>
2023-04-05 16:11:39 -0300

    Update aiflow-func.py

--- dags/aiflow-func.py
+++ dags/aiflow-func.py
@@ -1,82 +1,66 @@
-from datetime import timedelta
+# Simple example showing how the kubernetes POD operator can be used with airflow.
+# This example creates a DAG with four tasks: a dummy one, and  three more tasks
+# running in three separate PODs in a local kubernetes cluster.
+
+# import the necessary libraries
 from airflow import DAG
-from airflow.operators.bash_operator import BashOperator
-from airflow.utils.dates import days_ago
+from datetime import datetime, timedelta
+from airflow.contrib.operators.kubernetes_pod_operator import KubernetesPodOperator
+from airflow.operators.dummy_operator import DummyOperator
 
 default_args = {
     'owner': 'airflow',
     'depends_on_past': False,
-    'start_date': days_ago(2),
+    'start_date': datetime.utcnow(),
     'email': ['airflow@example.com'],
     'email_on_failure': False,
     'email_on_retry': False,
-    'retries': 1,
-    'retry_delay': timedelta(minutes=5),
-}
+    'retries': 0,
+    'retry_delay': timedelta(minutes=3)
+    }
 
-dag = DAG(
-    'airflow_fun',
-    default_args=default_args,
-    description='Show complex DAG in Airflow UI',
-    schedule_interval=timedelta(hours=1),
-    catchup=False,
-    max_active_runs=1,
+dag = DAG('k8s-sample', default_args=default_args, schedule_interval=timedelta(minutes=5))
+
+first_task = DummyOperator(task_id='first-task', dag=dag)
+
+# Create the tasks using images from dockerhub
+# Create a kubernetes pod using a python image
+python_task = KubernetesPodOperator(namespace='airflow-bigdata',
+                                    image="python:3.6",
+                                    cmds=["python","-c"],
+                                    arguments=["print('[PYTHON TASK] Hello world!')"],
+                                    labels={"foo": "bar"},
+                                    name="python-k8s-task",
+                                    task_id="python-task",
+                                    get_logs=True,
+                                    dag=dag
 )
 
-complex_dag_fun_1 = BashOperator(
-    task_id='starter',
-    bash_command='for i in {1..10}; do sleep 1 && echo "slept $i" E; done',
-    dag=dag,
+# Create a kubernetes pod using an ubuntu image
+bash_task = KubernetesPodOperator(namespace='airflow-bigdata',
+                                  image="ubuntu:16.04",
+                                  cmds=["echo"],
+                                  arguments=["[BASH TASK] Hi world!"],
+                                  labels={"foo": "bar"},
+                                  name="bash-k8s-task",
+                                  task_id="bash-task",
+                                  get_logs=True,dag=dag
 )
 
-complex_dag_fun_2 = BashOperator(
-    task_id='leaf_node1',
-    bash_command='for i in {1..5}; do sleep 1 && echo "slept $i" E; done',
-    dag=dag,
+# Create a kubernetes pod using a nodejs image
+node_task = KubernetesPodOperator(namespace='airflow-bigdata',
+                                  image="owncloudci/nodejs",
+                                  cmds=["node", "-e"],
+                                  arguments=["console.log('[NODEJS TASK] Hey world!')"],
+                                  labels={"foo": "bar"},
+                                  name="nodejs-k8s-task",
+                                  task_id="nodejs-task",
+                                  get_logs=True,
+                                  dag=dag
 )
 
-complex_dag_fun_3 = BashOperator(
-    task_id='leaf_node_2',
-    bash_command='for i in {1..10}; do sleep 1 && echo "slept $i" E; done',
-    dag=dag,
-)
 
-complex_dag_fun_4 = BashOperator(
-    task_id='bottleneck_node',
-    bash_command='for i in {1..1}; do sleep 1 && echo "slept $i" E; done',
-    dag=dag,
-)
-
-complex_dag_fun_5 = BashOperator(
-    task_id='terminal_node',
-    bash_command='for i in {1..10}; do sleep 1 && echo "slept $i" E; done',
-    dag=dag,
-)
-
-complex_dag_fun_6 = BashOperator(
-    task_id='parallel1',
-    bash_command='for i in {1..10}; do sleep 1 && echo "slept $i" E; done',
-    dag=dag,
-)
-
-complex_dag_fun_7 = BashOperator(
-    task_id='parallel2',
-    bash_command='for i in {1..10}; do sleep 1 && echo "slept $i" E; done',
-    dag=dag,
-)
-
-complex_dag_fun_8 = BashOperator(
-    task_id='parallel3',
-    bash_command='for i in {1..10}; do sleep 1 && echo "slept 1 sec the $i th time"; done',
-    dag=dag,
-)
-
-complex_dag_fun_1 >> complex_dag_fun_2
-complex_dag_fun_1 >> complex_dag_fun_3
-complex_dag_fun_2 >> complex_dag_fun_4
-complex_dag_fun_3 >> complex_dag_fun_4
-complex_dag_fun_4 >> complex_dag_fun_5
-
-complex_dag_fun_6 >> complex_dag_fun_5
-complex_dag_fun_7 >> complex_dag_fun_5
-complex_dag_fun_8 >> complex_dag_fun_5
+# Define the order of the tasks in the DAG
+python_task.set_upstream(first_task)
+bash_task.set_upstream(first_task)
+node_task.set_upstream(first_task)
