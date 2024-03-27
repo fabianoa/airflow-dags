6bbf89efe73c99342c54f2d739e5d80367b51914
Fabiano Alencar <fabiano.alencar@gmail.com>
2024-03-27 13:03:31 -0300

    Update sample-spark-dag.py

--- sample-spark-dag.py
+++ sample-spark-dag.py
@@ -32,7 +32,7 @@ t1 = SparkKubernetesOperator(
     application_file="/sample.yaml",
     kubernetes_conn_id="k8s",
     do_xcom_push=True,
-    namespace="kubedata-airflow",
+    namespace="spark-jobs",
     dag=dag,
 )
 
